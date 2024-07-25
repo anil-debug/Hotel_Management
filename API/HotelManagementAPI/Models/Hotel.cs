49edc4a45a43e3f47654775d618292aec4cf5e6b
Anil  Kumar <anik_kumar@solomon.com.tw>
2024-07-25 09:49:05 +0800

    Remove unnecessary files

--- API/HotelManagementAPI/Models/Hotel.cs
+++
@@ -1,7 +0,0 @@
-public class Hotel
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public ICollection<Room> Rooms { get; set; }
-}
